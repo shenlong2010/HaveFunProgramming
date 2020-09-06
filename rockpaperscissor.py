9828903a528a637e973905e08762ccac7fb7e9d9
Loc Lam <shenlong3010@gmail.com>
2020-09-06 15:33:38 -0400

    finalized and tested the rock paper scissor game

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,6 +1,6 @@
 # Loc - FunProgramming
 
-# Rock, Paper, Scissor, by Ak Sweigart
+# Rock, Paper, Scissor, by Al Sweigart
 # A program to play rock-paper-scissor
 
 import random, time, sys
@@ -20,4 +20,66 @@ while True: # main loop
 
 		if playerMove == 'R' or playerMove == 'P' or playerMove == 'S':
 			break
-	
+		else:
+			print('Type one of of R, P, S, or Q.')
+
+	if playerMove == 'R':
+		print('ROCK versus...')
+		playerMove = 'ROCK'
+	elif playerMove == 'P':
+		print('PAPER versus...')
+		playerMove = 'PAPER'
+	elif playerMove == 'S':
+		print('SCISSOR versus...')
+		playerMove = 'SCISSOR'
+
+	# print({'R': 'ROCK versus...',
+	# 	   'P': 'PAPER versus...',
+	# 	   'S': 'SCISSOR versus...'}[playerMove])
+
+	# Count to three with dramatic pauses
+	time.sleep(0.5)
+	print('1...')
+	time.sleep(0.25)
+	print('2...')
+	time.sleep(0.25)
+	print('3...')
+	time.sleep(0.25)
+
+	# Display what the computer chose
+	randomNumber = random.randint(1, 3) # or random.choice(['ROCK', 'PAPER', 'SCISSOR'])
+	if randomNumber == 1:
+		computerMove = 'ROCK'
+	elif randomNumber == 2:
+		computerMove = 'PAPER'
+	else:
+		computerMove = 'SCISSOR'
+	print(computerMove)
+
+	# Display and record the win/loss/tie:
+	# Tie condition
+	if playerMove == computerMove:
+		print('It\'s a tie!')
+		ties += 1
+
+	# Win condition
+	elif playerMove == 'ROCK' and computerMove == 'SCISSOR':
+		print('You win!')
+		wins += 1
+	elif playerMove == 'PAPER' and computerMove == 'ROCK':
+		print('You win!')
+		wins += 1
+	elif playerMove == 'SCISSOR' and computerMove == 'PAPER':
+		print('You win!')
+		wins += 1
+
+	# Lose condition
+	elif playerMove == 'ROCK' and computerMove == 'PAPER':
+		print('You lose!')
+		losses += 1
+	elif playerMove == 'PAPER' and computerMove == 'SCISSOR':
+		print('You lose!')
+		losses += 1
+	elif playerMove == 'SCISSOR' and computerMove == 'ROCK':
+		print('You lose!')
+		losses += 1
