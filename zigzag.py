cabcbd4c824864205de568a9f33a9e564f4dc360
Loc Lam <shenlong3010@gmail.com>
2020-09-06 16:09:06 -0400

    defined functions for zigzag, added main(), follow good guidelines for python coding

--- zigzag.py
+++ zigzag.py
@@ -6,26 +6,37 @@
 # Ctrl-C to stop the program
 import time
 
-# constant
+# constants
 constants = {
     "DELAY": 0.05,  # Pause for 50 milliseconds
     "INDENT_CHARACTER": " ",
     "SPECIAL_CHARACTER": "********",
 }
-
-indentSize = 0  # How many spaces to indent
-
-while True:  # main loop
-    # Zig to the right 20 times:
+    
+# Zig to the right 20 times:
+def zig(indentSize):
     for i in range(20):
         indentSize += 1
         indentation = constants["INDENT_CHARACTER"] * indentSize
         print(indentation + constants["SPECIAL_CHARACTER"])
         time.sleep(constants["DELAY"])
+    return indentSize
 
-    # Zag to the left 20 times:
+# Zag to the left 20 times:
+def zag(indentSize):
     for i in range(20):
         indentSize -= 1
         indentation = constants["INDENT_CHARACTER"] * indentSize
         print(indentation + constants["SPECIAL_CHARACTER"])
         time.sleep(constants["DELAY"])
+
+def main():
+    # How many spaces to indent
+    indentSize = 0
+    # main loop
+    while True:
+        nextIndext = zig(indentSize)
+        zag(nextIndext)
+
+if __name__ == '__main__':
+    main()
