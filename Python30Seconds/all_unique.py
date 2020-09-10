d3a22a9e7a32556864c97d7b39e684dbb61e50dc
Loc Lam <shenlong3010@gmail.com>
2020-09-10 00:30:25 -0400

    add doc to all_unique

--- Python30Seconds/all_unique.py
+++ Python30Seconds/all_unique.py
@@ -1,4 +1,13 @@
 def all_unique(lst):
+    """Return true if all the values in a list are unique, 
+    false otherwise.
+
+    Args:
+        lst (list): list of numbers
+
+    Returns:
+        boolean: return boolean if list are unique
+    """
     return len(lst) == len(set(lst))
 
 x = [1, 2, 3, 4, 5, 6]
