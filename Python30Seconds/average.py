0252d20ab2775bb4e90956c5e8da0e0480e0c545
Loc Lam <shenlong3010@gmail.com>
2020-09-10 21:15:51 -0400

    Refactor based on PR, add docstring to the function

--- Python30Seconds/average.py
+++ Python30Seconds/average.py
@@ -1,4 +1,9 @@
 def average(*args):
+    """Return the average of two or more numbers
+
+    Returns:
+        float: the average number
+    """
     return sum(args, 0.0) / len(args)
 
 print(average(*[1, 2, 3])) # 2.0
