2ec5ba6f8a7ff866d123efc0b4937e1516b91312
Loc Lam <shenlong3010@gmail.com>
2020-09-16 21:42:03 -0400

    create a program to convert hexadecimal to integer responding using base 16

---
+++ Python30Seconds/hex_to_rgb.py
@@ -0,0 +1,9 @@
+def hexToRgb(hex):
+    # int(x, base)
+    return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
+
+print(hexToRgb('FFA501'))
+
+print(int('FF', 16))
+print(int('A5', 16))
+print(int('01', 16))
